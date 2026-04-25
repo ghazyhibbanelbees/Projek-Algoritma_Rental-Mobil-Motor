3f8a1c917fbced5071d51c62a5e1cf77a5931aa4
adrianhermawan <adrianhcrmawan@gmail.com>
2026-04-25 10:12:56 +0700

    Add files via upload

---
+++ Projek Rental Mobil Motor.py
@@ -0,0 +1,169 @@
+# ==============================================
+# Kelompok 23
+# Nama:
+# Adrian Hermawan - J0403251044
+# Ghazyhibban Kumayl Elbees - J0403251
+# Kelas : B1
+# ==============================================
+
+# ==============================================
+# Projek Matakuliah Algoritma dan Struktur Data:
+# Rental Mobil dan Motor
+# ==============================================
+
+class Node:
+    def __init__(self, id, nama, jenis, harga, status = "Ready"):
+        self.id = id
+        self.nama = nama
+        self.status = status
+        self.jenis = jenis
+        self.harga = harga
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def tampilkan(self):
+        current = self.head
+        
+        if self.head is None:
+            print("Maaf, data masih kosong.")
+            return
+
+
+        while current is not None:
+            print(f"{current.id}. Nama Mobil: {current.nama}, Jenis:")
+            
+            current = current.next
+            
+    def tambah_kendaraan(self, id, nama, jenis, harga, status = "Ready"):
+        nodeK = Node(id, nama, jenis, harga, status)
+        
+        if self.head is None:
+            self.head = nodeK
+            self.tail = nodeK
+        else:
+            self.tail.next = nodeK
+            self.tail = nodeK
+        
+    def ubah_kendaraan(self, id):
+        current = self.head
+
+        #loop dari head ke tail
+        while current is not None:
+            #kalau ketemu idnya
+            if current.id == id:
+                print("Data ditemukan, masukkan data baru:")
+
+                #ganti data baru
+                current.nama = input("Nama baru: ")
+                current.jenis = input("Jenis baru: ")
+
+                try:
+                    current.harga = int(input("Harga baru: "))
+                except:
+                    print("Harga harus angka!")
+                    return
+
+                current.status = input("Status baru (Ready/Dipakai): ")
+            
+                print("Data berhasil diubah!")
+                return
+
+            current = current.next
+    
+        print("ID tidak ditemukan!")
+
+
+
+    def hapus_kendaraan(self, id):
+        current = self.head
+        prev = None
+
+        while current is not None:
+            if current.id == id:
+                # kasus: hapus head
+                if prev is None:
+                    self.head = current.next
+                    if current == self.tail:  # kalau cuma 1 node
+                        self.tail = None
+                else:
+                    prev.next = current.next
+                    if current == self.tail:  # hapus tail
+                        self.tail = prev
+
+                print("Kendaraan berhasil dihapus!")
+                return
+            
+            prev = current
+            current = current.next
+
+        print("ID tidak ditemukan!")
+        
+
+    def simpan_file(self, filename="dataKendaraan.txt"):
+        current = self.head
+    
+        with open(filename, "w") as file:
+            while current is not None:
+                file.write(f"{current.id}|{current.nama}|{current.jenis}|{current.harga}|{current.status}\n")
+                current = current.next
+
+    def load_file(self, filename="dataKendaraan.txt"):
+        try:
+            with open(filename, "r") as file:
+                for line in file:
+                    id, nama, jenis, harga, status = line.strip().split("|")
+                    self.tambah_kendaraan(id, nama, jenis, int(harga), status)
+        except FileNotFoundError:
+            print("File belum ada, mulai dari data kosong.")
+
+def main():
+    ll = LinkedList()
+    ll.load_file()
+    ll.tampilkan()
+    
+    while True:
+        print("\n===================== Rental Mobil dan Motor Babeh Ipul =====================")
+        print("1. Tambah Kendaraan")
+        print("2. Tampilkan Daftar Kendaraan")
+        print("3. Ubah Data Kendaraan")
+        print("4. Hapus Data Kendaraan")
+        print("0. Keluar")
+        
+        pilih = input("Pilih Menu (1-4): ")
+        if pilih == "1":
+            print("\n========Tambah Kendaraan========")
+
+            id = input("Masukkan id: ")
+            nama = input("Masukkan Nama Kendaraan: ")
+            jenis = input("Masukkan jenis (Mobil/Motor): ")
+
+            try:
+                harga = int(input("Masukkan harga sewa: "))
+            except:
+                print("Harga harus angka!")
+                continue
+
+            ll.tambah_kendaraan(id,nama,jenis,harga)
+            ll.simpan_file()
+            print("Kendaraan berhasil ditambahkan!")
+        elif pilih == "2":
+            ll.tampilkan()
+        elif pilih == "3":
+            print("\n======== Ubah Data Kendaraan ========")
+            id = input("Masukkan ID kendaraan yang ingin diubah: ")
+            ll.ubah_kendaraan(id)
+            ll.simpan_file()
+        elif pilih == "4":
+            print("\n======== Hapus Kendaraan ========")
+            id = input("Masukkan ID kendaraan yang ingin dihapus: ")
+            ll.hapus_kendaraan(id)
+            ll.simpan_file()
+        elif pilih == "0":
+            break
+
+if __name__ == "__main__":
+        main()
